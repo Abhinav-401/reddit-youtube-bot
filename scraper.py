98981065377f1ae82e3954a68f44d56ebbcbfbd4
Abhinav-401 <53551568+Abhinav-401@users.noreply.github.com>
2020-02-25 15:16:17 +0530

    Added Comments

--- scraper.py
+++ scraper.py
@@ -4,18 +4,19 @@ reddit = praw.Reddit(client_id='Q9MUOqXsxdmnqw',
                      client_secret='NPL9Cm-onUwKxxtaUFfkunP5Kv0', password='PlanetSplitter2526',
                      user_agent='RedditBot', username='TesterBester_Binary')
 
-subreddit = reddit.subreddit('python')
+subreddit = reddit.subreddit('python')                                                                  #Subreddit name that you want
 
 conversedict = {}
-hot_python = subreddit.hot(limit=3)
+hot_python = subreddit.hot(limit=3)                                                                     #Number of posts that you want to scrape, includes number of stickied posts as well
 
 for submission in hot_python:
-    if not submission.stickied:
+    if not submission.stickied:                                                                         #Will give only one post as limit is 3 and number of stickied posts are 2 
         print('Title: {}, ups: {}, downs: {}, Have we visited?: {}, subid: {}'.format(submission.title,
                                                                                                    submission.ups,
                                                                                                    submission.downs,
                                                                                                    submission.visited,
                                                                                                     submission.id))
+#Below line opens up the load more option, which otherwise gives error                                                                                                    
         submission.comments.replace_more(limit=None)      #limit=0                                                                                        
         comments = submission.comments
         for comment in comments:
